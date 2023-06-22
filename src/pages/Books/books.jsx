2315850a7e24a08100241bcf361602672508e9e0
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-22 18:40:06 +0200

    books en cours

--- src/pages/Books/books.jsx
+++ src/pages/Books/books.jsx
@@ -6,6 +6,7 @@ import './books.css'
 const BooksSearch = () => {
   const [books, setBooks] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+
 const REACT_APP_API_KEY = 'AIzaSyBoB6wZ0fqhwPsVGDe6QQZ6sDyFjZ5y4Hc'
   useEffect(() => {
     const fetchBooks = async () => {
@@ -35,33 +36,39 @@ const REACT_APP_API_KEY = 'AIzaSyBoB6wZ0fqhwPsVGDe6QQZ6sDyFjZ5y4Hc'
   return (
     <div>
       <h2>Book Search</h2>
-      <form onSubmit={handleSearch}>
+      <form onSubmit={handleSearch} class="search-form">
         <input
           type="text"
           value={searchTerm}
           onChange={(event) => setSearchTerm(event.target.value)}
           placeholder="Enter a book title or keyword"
-        />
-        <button type="submit">Search</button>
+          class="search-input-left"/>
+        <button type="submit" class="rounded-button-right">Rechercher</button>
       </form>
-      <h2>Search Results</h2>
-      <div class="container" id="books-grid">
-      {books.map((book) => (
-        <div class="card" key={book.id}>
-          <h3>{book.volumeInfo.title}</h3>
-          {book.volumeInfo.imageLinks && (
-            <img
-              src={book.volumeInfo.imageLinks.thumbnail}
-              alt={book.volumeInfo.title}
-            />
-          )}
-          <p>{book.volumeInfo.authors && book.volumeInfo.authors.join(', ')}</p>
-          <p>{book.volumeInfo.description}</p>
-        </div>
-      ))}
+
+      <h2>Les petits trésors cachés:</h2>
+      <div className="container">
+        {books.map((book) => (
+          <div className="card" key={book.id}>
+            <h3>{book.volumeInfo.title}</h3>
+            {book.volumeInfo.imageLinks && (
+              <img
+                src={book.volumeInfo.imageLinks.thumbnail}
+                alt={book.volumeInfo.title}
+              />
+            )}
+            <p>{book.volumeInfo.authors && book.volumeInfo.authors.join(', ')}</p>
+            <p className="description">
+              {book.volumeInfo.description && book.volumeInfo.description.length > 150
+                ? `${book.volumeInfo.description.slice(0, 150)}...`
+                : book.volumeInfo.description}
+           </p>  
+          </div>
+        ))}
       </div>
     </div>
-  );
+);
+
 };
 
 export default BooksSearch;
