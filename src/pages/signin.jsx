f1b0780beb0fab44af29c619a10b602a41386163
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-06-22 11:41:10 +0200

    LD adding logout button for now

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,9 +1,11 @@
 import SigninForm from "../components/signinform/index";
+import Logout from "../components/logout/index";
 
 function SignIn() {
   return (
     <>
       <SigninForm />
+      <Logout />
     </>
   );
 }
