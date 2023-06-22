0252e46bc71252d5d079f88daa1f328187e20c62
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-22 14:13:24 +0200

    books reasearch + start css

---
+++ src/pages/Books/books.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+// import dotenv from 'dotenv';
+import './books.css'
+// dotenv.config();
+
+const BooksSearch = () => {
+  const [books, setBooks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+const REACT_APP_API_KEY = 'AIzaSyBoB6wZ0fqhwPsVGDe6QQZ6sDyFjZ5y4Hc'
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch(
+          `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${REACT_APP_API_KEY}`
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setBooks(data.items);
+        } else {
+          console.error('Error:', response.status);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchBooks();
+  }, [searchTerm]);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    fetchBooks();
+  };
+
+  return (
+    <div>
+      <h2>Book Search</h2>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          placeholder="Enter a book title or keyword"
+        />
+        <button type="submit">Search</button>
+      </form>
+      <h2>Search Results</h2>
+      <div class="container" id="books-grid">
+      {books.map((book) => (
+        <div class="card" key={book.id}>
+          <h3>{book.volumeInfo.title}</h3>
+          {book.volumeInfo.imageLinks && (
+            <img
+              src={book.volumeInfo.imageLinks.thumbnail}
+              alt={book.volumeInfo.title}
+            />
+          )}
+          <p>{book.volumeInfo.authors && book.volumeInfo.authors.join(', ')}</p>
+          <p>{book.volumeInfo.description}</p>
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+};
+
+export default BooksSearch;
