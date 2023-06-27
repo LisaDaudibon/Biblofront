2d8b7533dcd03699b8f7a9a2c0634a089a9aaae6
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-27 17:09:55 +0200

    add Vite-plugin-ruby

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import ViteRuby from 'vite-plugin-ruby'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteRuby(),
+  ],
 })
+
