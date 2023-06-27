4782e771eb1793cd7bee1ab0e67c28914ac0defe
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-27 12:52:32 +0200

    books index with research

--- src/pages/Books/books.jsx
+++ src/pages/Books/books.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DOMPurify from 'dompurify';
 import '../Books/books.css';
+import BookDetails from './book';
 
 const Books = () => {
   const [books, setBooks] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedBook, setSelectedBook] = useState(null);
+  const [hoveredBook, setHoveredBook] = useState(null);
 
-  const REACT_APP_API_KEY = 'AIzaSyBw9dOsg8QRJ0FtafUZ29mSbpsdg7nNAoY'; // Replace with your Google Books API key
+  const REACT_APP_API_KEY = 'AIzaSyBw9dOsg8QRJ0FtafUZ29mSbpsdg7nNAoY';
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -18,13 +21,15 @@ const Books = () => {
           return;
         }
 
-        const response = await fetch(
-          `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(sanitizedSearchTerm)}&key=${REACT_APP_API_KEY}+&maxResults=40`
-        );
+        const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
+          sanitizedSearchTerm
+        )}&key=${REACT_APP_API_KEY}&maxResults=40`;
+
+        const response = await fetch(apiUrl);
 
         if (response.ok) {
           const data = await response.json();
-          setBooks(data.items);
+          setBooks(data.items || []);
         } else {
           console.error('Error:', response.status);
         }
@@ -44,46 +49,76 @@ const Books = () => {
     setSearchTerm(sanitizedSearchTerm);
   };
 
-  return (
-    <div>
-      <h2>Book Search</h2>
-      <form onSubmit={handleSearch} className="search-form">
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(event) => setSearchTerm(event.target.value)}
-          placeholder="Enter a book title or keyword"
-          className="search-input-left"
-        />
-        <button type="submit" className="rounded-button-right">
-          Rechercher
-        </button>
-      </form>
+  const handleBookHover = (book) => {
+    setHoveredBook(book);
+  };
 
-      <h2>Les petits trésors cachés:</h2>
+ const handleBookClick = (book) => {
+  setSelectedBook(book);
+};
+
+
+  const handleCloseDetails = () => {
+    setSelectedBook(null);
+  };
+
+  const renderBooks = () => {
+    return (
       <div className="container">
         {books
-          .filter((book) => book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail)
+          .filter(
+            (book) =>
+              book.volumeInfo &&
+              book.volumeInfo.imageLinks &&
+              book.volumeInfo.imageLinks.thumbnail
+          )
           .map((book) => (
-            <div className="card" key={book.id}>
+            <div
+              className="card"
+              key={book.id}
+              onMouseEnter={() => handleBookHover(book)}
+              onMouseLeave={() => setHoveredBook(null)}
+              onClick={() => handleBookClick(book)}
+            >
               <div className="card-content">
+                <img
+                  src={book.volumeInfo.imageLinks.thumbnail}
+                  alt={book.volumeInfo.title}
+                />
                 <h3>{book.volumeInfo.title}</h3>
-                {book.volumeInfo.imageLinks && (
-                  <img
-                    src={book.volumeInfo.imageLinks.thumbnail}
-                    alt={book.volumeInfo.title}
-                  />
-                )}
-                <p>{book.volumeInfo.authors && book.volumeInfo.authors.join(', ')}</p>
-                <p className="description">
-                  {book.volumeInfo.description && book.volumeInfo.description.length > 250
-                    ? `${book.volumeInfo.description.slice(0, 250)}...`
-                    : book.volumeInfo.description}
-                </p>
               </div>
             </div>
           ))}
       </div>
+    );
+  };
+
+  return (
+    <div className="page-container">
+      <h2>Book Search</h2>
+      <div className="navbar">
+        <form onSubmit={handleSearch} className="search-form">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+            placeholder="Enter a book title or keyword"
+            className="search-input-left"
+          />
+          <button type="submit" className="rounded-button-right">
+            Search
+          </button>
+        </form>
+      </div>
+
+      <h2>Search Results:</h2>
+      {renderBooks()}
+
+      {selectedBook && (
+        <div className="popup">
+          <BookDetails book={selectedBook} onCloseDetails={handleCloseDetails} />
+        </div>
+      )}
     </div>
   );
 };
