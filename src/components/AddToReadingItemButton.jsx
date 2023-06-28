fd30e44887d749b67ec9fd68b77f539893c11db4
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-28 21:25:02 +0200

    add button reading item

--- src/components/AddToReadingItemButton.jsx
+++ src/components/AddToReadingItemButton.jsx
@@ -1,23 +1,67 @@
-// import { useEffect } from "react";
-// import { useSetAtom } from 'jotai';
-// import { useAtomValue } from 'jotai';
-// import { userTokenAtom } from '../../atoms/userTokenAtom';
-// import { userIdAtom } from '../../atoms/userIdAtom';
-// import { loggedInAtom } from '../../atoms/loggedInAtom';
+import React, { useEffect } from 'react';
+import { useSetAtom, useAtomValue } from 'jotai';
+import { bookTitleAtom } from '../atoms/bookTitleAtom';
+import { bookAuthorAtom } from '../atoms/bookAuthorAtom';
+import { bookPublishedAtom } from '../atoms/bookPublishedAtom';
+import { bookPagesAtom } from '../atoms/bookPagesAtom';
+import { bookCategoryAtom } from '../atoms/bookCategoryAtom';
 
+const AddToReadingItem = ({ ReadingItem }) => {
+  const bookTitle = useAtomValue(bookTitleAtom)
+  const setBookTitle = useSetAtom(bookTitleAtom);
+  const bookAuthor = useAtomValue(bookAuthorAtom)
+  const setBookAuthor = useSetAtom(bookAuthorAtom);
+  const bookPublishedDate = useAtomValue(bookPublishedAtom)
+  const setBookPublished = useSetAtom(bookPublishedAtom);
+  const bookPages = useAtomValue(bookPagesAtom)
+  const setBookPages = useSetAtom(bookPagesAtom);
+  const bookCategory = useAtomValue(bookCategoryAtom)
+  const setBookCategory = useSetAtom(bookCategoryAtom);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError('');
+    setSuccess('');
 
-// const AddToReadingItem = ({ ReadingItem }) => {
+    try {
+      const response = await fetch('https://bibloback.fly.dev/books', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          book: {
+            title: title,
+            author: author,
+            description: description,
+            image_url: image_url,
+            pages: pages,
+            published_date: published_date,
+            category: category,
+          }
+        }),
+      });
 
-//   const usertoken = useAtomValue(userTokenAtom);
-//   const userid = useAtomValue(userIdAtom)
-//   const loggedIn = useAtomValue(loggedInAtom);
-//   const setUsertoken = useSetAtom(userTokenAtom);
-//   const setUserid = useSetAtom(userIdAtom)
+      if (response.ok) {
+        const token = await response.headers.get("Authorization");
+        setUsertoken(token);
+        const responseData = await response.json();
+        setUserid(responseData.user.id);
 
+        setSuccess('Compte créé avec succès!'); // Set success flash message
+      } else {
+        setError("Erreur lors de l'enregistrement du compte");
+      }
+    } catch (error) {
+      setError('Erreur lors de la création du compte');
+    }
+    // <disconnectUser /> aller chercher le code dans la branche getmembers, code à retravailler
+  };
 
-//     return(
+     return(
 
-//     )
+     )
 
-//   }
+   }
