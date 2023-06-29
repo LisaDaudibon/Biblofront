b931af6e4bee2f154a1c627038a957f7adb4b558
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-06-29 10:49:32 +0200

    LD starting edit button get member

--- src/components/getmember/editmemberbutton.jsx
+++ src/components/getmember/editmemberbutton.jsx
@@ -6,60 +6,86 @@ import { loggedInAtom } from '../../atoms/loggedInAtom';
 import { useState } from 'react';
 
 function Editmemberbutton() {
-  const usertoken = useAtomValue(userTokenAtom);
-  const userid = useAtomValue(userIdAtom);
-  const loggedIn = useAtomValue(loggedInAtom);
-  const setUsertoken = useSetAtom(userTokenAtom);
-  const setUserid = useSetAtom(userIdAtom);
+  // const iseditingemail = 
+  // const usertoken = useAtomValue(userTokenAtom);
+  // const userid = useAtomValue(userIdAtom);
+  // const loggedIn = useAtomValue(loggedInAtom);
+  // const setUsertoken = useSetAtom(userTokenAtom);
+  // const setUserid = useSetAtom(userIdAtom);
 
-  const [editemail, setEditemail] = useState;
-  const [editpseudo, setEditpseudo]= useState;
-  const [editpassword, setEditpassword]=useState;
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
-
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const response = await fetch(`https://bibloback.fly.dev/users/${userid}`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `${usertoken}`,
-        },
-        body: JSON.stringify({
-          user: {
-            email: email,
-            pseudo: pseudo,
-            password: password,
-          }
-        }),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-
-        setEditemail(data.user.email)
-        setEditpseudo(data.user.pseudo)
-        setEditpassword(data.user.password)
-
-        setSuccess('Edition réussie!');
-      } else {
-        setError('error')
-      }
-    } catch (error) {
-      setError('Erreur lors de la tentative de connection!');
-    }
-  };
+ 
+  // const [editpseudo, setEditpseudo]= useState;
+  // const [editpassword, setEditpassword]=useState;
+  // const [error, setError] = useState('');
+  // const [success, setSuccess] = useState('');
 
 
-  return (
-    <div>
-      <button type="submit">Editer les infos</button> 
-    </div>
-  )
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+
+  //   try {
+  //     const response = await fetch(`https://bibloback.fly.dev/users/${userid}`, {
+  //       method: 'PATCH',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'Authorization': `${usertoken}`,
+  //       },
+  //       body: JSON.stringify({
+  //         user: {
+  //           email: email,
+  //           pseudo: pseudo,
+  //           password: password,
+  //         }
+  //       }),
+  //     });
+
+  //     if (response.ok) {
+  //       const data = await response.json();
+
+  //       setEditemail(data.user.email)
+  //       setEditpseudo(data.user.pseudo)
+  //       setEditpassword(data.user.password)
+
+  //       setSuccess('Edition réussie!');
+  //     } else {
+  //       setError('error')
+  //     }
+  //   } catch (error) {
+  //     setError('Erreur lors de la tentative de connection!');
+  //   }
+  // // };
+
+  // const [editemail, setEditemail] = useState('');
+  // const [sanitizedemail, setSanitizedemail] = useState('');
+
+  // useEffect(() => {
+  //   const handleemail = () => {
+  //     const sanitizedemail = DOMPurify.sanitize(email)}
+  // })
+
+  // const onSubmit = (event) => {
+  //   event.preventDefault();
+
+  //   const sanitizedemail = DOMPurify.sanitize(email)
+
+  //   setSanitizedemail = editemail
+  // }
+
+  // return (
+    
+      // {if iseditingemail? (<div>
+  //     <>
+  //     <input
+  //     type="email"
+  //     id="email"
+  //     value={setSanitizedemail}
+  //     placeholder="email"
+  //     onChange={(e) => setSanitizedemail(e.target.value)}
+  //   /></>
+  // {/* //   // </div>): ( <div>
+  //   //   <button type="submit">Editer les infos</button> 
+  //   // </div>)} */}
+  {/* // ) */}
 }
 
 export default Editmemberbutton;
--- src/components/getmember/getmember.jsx
+++ src/components/getmember/getmember.jsx
@@ -4,6 +4,7 @@ import { userTokenAtom } from '../../atoms/userTokenAtom';
 import { loggedInAtom } from '../../atoms/loggedInAtom';
 import Logout from '../logout/logout';
 import './getmember.css';
+import Editmemberbutton from './editmemberbutton';
 
 function Getmember() {
   const usertoken = useAtomValue(userTokenAtom);
@@ -13,6 +14,11 @@ function Getmember() {
   const [profilpassword, setProfilpassword] = useState("");
   const [error, setError] = useState('');
   const [success, setSuccess] = useState('');
+
+  const [editemail, setEditemail] = useState('');
+  const [editpseudo, setEditpseudo]= useState('');
+  const [editpassword, setEditpassword]=useState('');
+  const [iseditingemail, setisEditingemail] = useState(false);
 
   useEffect(() => {
     setError ('');
@@ -45,6 +51,60 @@ function Getmember() {
 
   }, [usertoken, profilemail]);
 
+  // useEffect(() => {
+  //   setisEditingemail(true)
+  // })
+
+  const toggleeditbutton = () => {
+    if (iseditingemail === true) {
+      setisEditingemail(false);
+      return;
+    } else {
+      setisEditingemail(true);
+      return;
+    }
+  }
+
+  // const editmemberbutton = () => {
+
+
+  //   return()
+  // }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(`https://bibloback.fly.dev/users/${userid}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `${usertoken}`,
+        },
+        body: JSON.stringify({
+          user: {
+            email: `${editemail}`,
+            pseudo: pseudo,
+            password: password,
+          }
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+
+        setEditemail(data.user.email)
+        setEditpseudo(data.user.pseudo)
+        setEditpassword(data.user.password)
+
+        setSuccess('Edition réussie!');
+      } else {
+        setError('error')
+      }
+    } catch (error) {
+      setError('Erreur lors de la tentative de connection!');
+    }
+  };
 
   return (
     <>
@@ -54,9 +114,22 @@ function Getmember() {
         {success && <p>{success}</p>}
         <p id="profilecardtitle">Hello {profilpseudo} </p>
         <div id='profilecardinfo'>
-          Email : {profilemail}
+          Email :  {iseditingemail? (<>
+            <input
+            type="email"
+            id="email"
+            value={editemail}
+            placeholder="email"
+            onChange={(e) => setEditemail(e.target.value)}
+            />
+            <button type="submit">Sauvegarder</button>
+            <button onClick={toggleeditbutton}>Editer</button> </>
+          ) : (<>
+          {profilemail}
+          <button onClick={toggleeditbutton}>Editer</button></>)}
+
           <br></br>
-          <br></br>
+          <br></br> ;
           Pseudo : {profilpseudo}
           <br></br>
           <br></br>
