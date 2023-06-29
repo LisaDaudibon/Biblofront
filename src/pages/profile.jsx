2e7ddc49638a2390e4897abffa775b43cc08b306
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-29 18:02:59 +0200

    profile import

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,21 +1,25 @@
-import Getmember from "../components/getmember/getmember";
+import React from 'react';
 import { useAtomValue } from 'jotai';
-import { loggedInAtom } from "../atoms/loggedInAtom";
-import SignIn from "./signin";
+import { loggedInAtom } from '../atoms/loggedInAtom';
+import ReadingList from '../components/ReadingList';
+import Getmember from '../components/getmember/getmember';
+import SigninForm from '../components/signinform/signinform'; // Import the SignIn component
 
-function Profile() {
+const Profile = () => {
   const loggedIn = useAtomValue(loggedInAtom);
 
   return (
     <>
-      {loggedIn? (
+      {loggedIn ? (
+        <>
           <Getmember />
+          <ReadingList />
+        </>
       ) : (
-        <SignIn />
-        )
-      }
+        <SigninForm /> 
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
