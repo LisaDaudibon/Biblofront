322636503a7df7c74799ad61f9ccb5b88166aa36
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-30 14:17:37 +0200

    icon footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,31 +1,73 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 import LogoFooter from './footerLogo';
 import './footer.css';
 
 const Footer = () => {
+  const isSmallDevice = window.innerWidth <= 600; // Adjust the breakpoint as per your requirements
+
   return (
-    <footer>
+    <footer className="footer">
       <div className="footer-container">
         <div className="footer-content">
           <div className="footer-logo">
-          </div>
-          <div className="footer-links">
+            <LogoFooter />
+            <div className="footer-links">
             <ul>
-              <LogoFooter/>
-              <li><a href="/books">Livres</a></li>
-              <li><a href="/profile">Profile</a></li>
               <li>
-                <a href="/about">Contact :</a>
-                <a href="mailto:bibliophilea@yopmail.com">bibliophilea@yopmail.com</a>
-              </li> 
-              <li id="team-links"><a>Equipe:</a></li>
+                <a href="/books">
+                  {isSmallDevice ? (
+                    <FontAwesomeIcon icon={faBook} />
+                  ) : (
+                    <>
+                      <FontAwesomeIcon icon={faBook} />
+                      <span>Books</span>
+                    </>
+                  )}
+                </a>
+              </li>
               <li>
-                <a href="https://github.com/LisaDaudibon">LisaDaudibon</a>
-                <a href="https://github.com/KarineDHoshi">KarineDHoshi</a>
+                <a href="/profile">
+                  {isSmallDevice ? (
+                    <FontAwesomeIcon icon={faUser} />
+                  ) : (
+                    <>
+                      <FontAwesomeIcon icon={faUser} />
+                      <span>Profile</span>
+                    </>
+                  )}
+                </a>
+              </li>
+              <li>
+                <a href="mailto:bibliophilea@yopmail.com">
+                  {isSmallDevice ? (
+                    <FontAwesomeIcon icon={faEnvelope} />
+                  ) : (
+                    <>
+                      <FontAwesomeIcon icon={faEnvelope} />
+                      <span>Contact</span>
+                    </>
+                  )}
+                </a>
+              </li>
+              <li id="team-links">
+                <a href="#">
+                  {isSmallDevice ? (
+                    <FontAwesomeIcon icon={faGithubSquare} />
+                  ) : (
+                    <>
+                      <FontAwesomeIcon icon={faGithubSquare} />
+                      <span>Team</span>
+                    </>
+                  )}
+                </a>
               </li>
             </ul>
-         
           </div>
+          </div>
+          
         </div>
       </div>
     </footer>
