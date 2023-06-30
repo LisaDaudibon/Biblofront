383e0ee0cad5a7c6a65b1385755a3e53416a8ef2
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-30 16:45:53 +0200

    presentation du site

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
+import './contact.css';
 
 const About = () => {
   return (
-    <div className="container">
-      <h1 className="display-4">À propos de Bibliophilea</h1>
-      <p className="lead">Bibliophilea est une équipe de passionnés de livres qui met tout en œuvre pour créer une plateforme de lecture des plus agréables pour les utilisateurs.</p>
-      <p>
-        Notre objectif principal est de vous offrir une expérience de lecture exceptionnelle. De la conception à la réalisation de notre plateforme, nous nous engageons à fournir des fonctionnalités conviviales et intuitives qui rendront votre exploration des livres encore plus plaisante.
-      </p>
-      <p>
-        Nous comprenons l'importance de l'immersion dans un monde littéraire captivant, c'est pourquoi nous mettons tout en œuvre pour vous offrir une interface élégante, des recommandations personnalisées et une communauté de lecteurs passionnés avec qui vous pourrez partager votre amour des livres.
-      </p>
-      <p>
-        Rejoignez-nous dans cette aventure littéraire et découvrez un univers de lectures passionnantes qui vous transportera à chaque page tournée.
-      </p>
-      <p>
-        Vous pouvez nous contacter à l'adresse e-mail : <a href="mailto:bibliophilea@yopmail.com">bibliophilea@yopmail.com</a>
-      </p>
+    <div className="card">
+      <div className="card-header-about">
+        <h1 className="card-title-about">À propos de Bibliophilea</h1>
+      </div>
+      <div className="card-body-about">
+        <p className="card-text lead-about">
+          Bienvenue sur notre plateforme dédiée aux passionnés de livres, créée avec amour par trois fervents amoureux de la lecture ! Notre équipe est composée de véritables passionnés qui ont décidé de mettre leur amour des livres au service de la communauté littéraire en créant cet espace unique.
+        </p>
+        <p className="card-text-about">
+          En tant que passionnés de livres nous-mêmes, nous comprenons l'excitation de plonger dans de nouveaux mondes imaginaires, d'explorer des idées inspirantes et d'être transporté par les mots. Notre objectif est de partager cette passion avec vous et de créer une communauté dynamique où les amoureux des livres peuvent se retrouver.
+        </p>
+        <p className="card-text-about">
+          Nous avons conçu cette plateforme avec une attention particulière aux besoins des lecteurs. Chaque fonctionnalité, chaque outil et chaque détail a été soigneusement réfléchi pour offrir la meilleure expérience possible. Nous avons puisé dans notre propre expérience de lecteurs pour créer un espace convivial, intuitif et enrichissant.
+        </p>
+        <p className="card-text-about">
+          Notre amour pour la lecture transparaît dans chaque aspect de notre plateforme. Nous avons passé de nombreuses heures à sélectionner, classer et organiser les livres dans notre base de données, en veillant à inclure des titres variés et captivants pour tous les goûts. Notre équipe est également constamment à l'affût des nouvelles parutions et des tendances littéraires afin de maintenir notre collection à jour.
+        </p>
+        <p className="card-text-about">
+          Nous comprenons l'importance de partager ses découvertes littéraires et d'échanger avec d'autres lecteurs passionnés. C'est pourquoi nous avons créé un espace communautaire où vous pourrez interagir avec d'autres membres, discuter de vos livres préférés, partager des recommandations et trouver de nouvelles inspirations.
+        </p>
+        <p className="card-text-about">
+          Nous sommes fiers de pouvoir offrir cette plateforme à tous les amoureux des livres qui partagent notre passion. Rejoignez-nous et découvrez un espace conçu par des passionnés, pour des passionnés. Ensemble, partageons notre amour de la lecture et créons une communauté vibrante autour des livres qui nous font rêver, réfléchir et grandir.
+        </p>
+        <p>Vous pouvez nous contacter à l'adresse e-mail : <a href="mailto:bibliophilea@yopmail.com">bibliophilea@yopmail.com</a></p>
+      </div>
     </div>
   );
 };
