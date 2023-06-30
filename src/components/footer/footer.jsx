1f19703ccdf5c2a9fbcb21535d2577c25ebeca4b
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-30 11:23:09 +0200

    footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
-import Logo from './footerLogo';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookOpen } from '@fortawesome/free-solid-svg-icons';
+import LogoFooter from './footerLogo';
 import './footer.css';
 
 const Footer = () => {
-
   return (
     <footer>
       <div className="footer-container">
         <div className="footer-content">
           <div className="footer-logo">
-          <Logo/>
-
-          {/* <FontAwesomeIcon icon={faBookOpen} className="icon"/> */}
-            </div>
+          </div>
           <div className="footer-links">
             <ul>
+              <LogoFooter/>
               <li><a href="/books">Livres</a></li>
               <li><a href="/profile">Profile</a></li>
-              <li><a href="/about">Contact :</a> <a href="mailto:bibliophilea@yopmail.com">bibliophilea@yopmail.com</a> </li>
-              
+              <li>
+                <a href="/about">Contact :</a>
+                <a href="mailto:bibliophilea@yopmail.com">bibliophilea@yopmail.com</a>
+              </li> 
+              <li id="team-links"><a>Equipe:</a></li>
+              <li>
+                <a href="https://github.com/LisaDaudibon">LisaDaudibon</a>
+                <a href="https://github.com/KarineDHoshi">KarineDHoshi</a>
+              </li>
             </ul>
+         
           </div>
         </div>
       </div>
