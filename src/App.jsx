4351d062334e01050ecbcb31ce6fb89ebf71c76c
KarineDHoshi <karinedhoshi@yahoo.com>
2023-07-01 15:34:44 +0200

    error mp4 video

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Books from './pages/Books/books';
 import Navbar from './components/navbar/navbar';
 import About from './components/contact/contact';
 import Footer from './components/footer/footer';
+import PageNotFound from './pages/404/404error';
 import './App.css';
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/books" element={<Books />} />
           <Route path="/about" element={<About />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
         <Footer />
       </BrowserRouter>
